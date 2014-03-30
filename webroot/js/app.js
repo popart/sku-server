82ed657d9e12c6b3639d18ba38af5f35dbe7619f
Andrew Wong <wongwong.wong@gmail.com>
2014-03-30 12:51:05 -0400

    angular: GET by id

--- webroot/js/app.js
+++ webroot/js/app.js
@@ -6,4 +6,11 @@ var app = angular.module('app', [
 
 app.controller('SkuCtrl', ['$scope', 'Sku', function($scope, Sku) {
     $scope.skus = Sku.query()
+    Sku.query(
+        {'did': 1}, 
+        function(response){
+            $scope.sku_uno=response[0]
+            console.log(response[0])
+        }
+    )
 }])
