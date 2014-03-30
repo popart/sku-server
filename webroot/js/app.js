53bb28ee5721212ff8fa6d548411f51c18ecbd85
Andrew Wong <wongwong.wong@gmail.com>
2014-03-30 14:06:19 -0400

    File Upload base64

--- webroot/js/app.js
+++ webroot/js/app.js
@@ -6,11 +6,14 @@ var app = angular.module('app', [
 
 app.controller('SkuCtrl', ['$scope', 'Sku', function($scope, Sku) {
     $scope.skus = Sku.query()
-    Sku.query(
-        {'did': 1}, 
-        function(response){
-            $scope.sku_uno=response[0]
-            console.log(response[0])
-        }
-    )
+    $scope.select = function(did) {
+        //$scope.selected = did
+        Sku.query(
+            {'did': did}, 
+            function(response){
+                $scope.selected=response[0]
+                console.log(response[0])
+            }
+        )
+    }
 }])
