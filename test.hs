fd562d75d165d3dff575350372bd647d11c70bdc
Andrew Wong <wongwong.wong@gmail.com>
2014-03-31 00:40:11 -0600

    Delete test.hs

--- test.hs
+++
@@ -1,6 +0,0 @@
-{-# LANGUAGE OverloadedStrings #-}
-import Style
-import Data.Aeson
-import Data.Maybe
-
-test = description $ fromJust (decode "{\"description\":\"andy\", \"color\":\"blue\", \"size\":\"28w\", \"photo\":\"asdfasdf\"}" :: Maybe Style)
